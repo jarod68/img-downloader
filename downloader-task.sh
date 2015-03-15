0c7ab52c47dcb40975d184a240dd7b523dbad0b3
Matthieu Holtz <matthieu.holtz@gmail.com>
2015-03-15 17:26:48 +0100

    + uncomment raspbian and ubuntu

--- downloader-task.sh
+++ downloader-task.sh
@@ -7,12 +7,12 @@
 
 LOG_FILE="./downloader-task.log"
 
-#raspbian=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/raspbian_latest" -r "raspbian" -p`
+raspbian=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/raspbian_latest" -r "raspbian" -p`
 openelec=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/openelec_latest" -r "openelec" -p`
-#ubuntu=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/ubuntu_latest" -r "ubuntu" -p`
+ubuntu=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/ubuntu_latest" -r "ubuntu" -p`
 
-#echo "$raspbian" >> $LOG_FILE
+echo "$raspbian" >> $LOG_FILE
 echo "$openelec" >> $LOG_FILE
-#echo "$ubuntu" >> $LOG_FILE
+echo "$ubuntu" >> $LOG_FILE
 
 exit 0
