1622f3d643a70fb1ae2cc7ec09309da25099ddfa
Matthieu Holtz <matthieu.holtz@gmail.com>
2015-03-15 17:09:42 +0100

    + update downloader-task script to output log file

--- downloader-task.sh
+++ downloader-task.sh
@@ -5,8 +5,14 @@
 # Year:   2015
 # ---------------------------------------------------------------------------
 
-bash ./downloader.sh -u "http://downloads.raspberrypi.org/raspbian_latest" -r "raspbian" -v
-bash ./downloader.sh -u "http://downloads.raspberrypi.org/openelec_latest" -r "openelec" -v
-bash ./downloader.sh -u "http://downloads.raspberrypi.org/ubuntu_latest" -r "ubuntu" -v
+LOG_FILE="./downloader-task.log"
+
+#raspbian=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/raspbian_latest" -r "raspbian" -p`
+openelec=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/openelec_latest" -r "openelec" -p`
+#ubuntu=`date "+DATE: %Y-%m-%d - TIME: %H:%M:%S"`" - LOG: "`bash ./downloader.sh -u "http://downloads.raspberrypi.org/ubuntu_latest" -r "ubuntu" -p`
+
+#echo "$raspbian" >> $LOG_FILE
+echo "$openelec" >> $LOG_FILE
+#echo "$ubuntu" >> $LOG_FILE
 
 exit 0
